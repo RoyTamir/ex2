00a4e7f3e6ca52a044a2814d77721296ccc15d1c
root <root@DESKTOP-HKCE1KB.localdomain>
2020-08-22 15:20:20 +0300

    Making the implemention in MatrixClass.cpp shorter

--- src/MatrixClass.cpp
+++ src/MatrixClass.cpp
@@ -7,25 +7,13 @@ PMatrix matrix;
 //methods
 public:
 	MatrixClass(uint32_t height, uint32_t width) throw(ErrorCodeException) {
-        //creating the matrix
-        ErrorCode er = matrix_create(&matrix, height, width);
-
-        //checking success
-        ErrorCodeException* exeption = new ErrorCodeException(er);
-        if(!exeption->isSuccess()) {
-            throw exeption;
-        }
+        //creating the matrix & throwing exception if needed
+        ErrorCodeException::throwErrorIfNeeded(matrix_create(&matrix, height, width));
     }
 
     MatrixClass(const MatrixClass& source) throw(ErrorCodeException) {
-        //copy the matrix
-        ErrorCode er = matrix_copy(&matrix, source.matrix);
-
-        //checking success
-        ErrorCodeException* exeption = new ErrorCodeException(er);
-        if(!exeption->isSuccess()) {
-            throw exeption;
-        }
+        //copy the matrix & throwing exception if needed
+        ErrorCodeException::throwErrorIfNeeded(matrix_copy(&matrix, source.matrix));
     }
 
     MatrixClass& operator=(const MatrixClass& source) throw(ErrorCodeException){
@@ -33,15 +21,18 @@ public:
         //Trying to destroy the matrix in the field (if not intalized yet would do nothing)
         matrix_destroy(matrix);
 
-        //create copy of the matrix in the fields
-        ErrorCode er = matrix_copy(&matrix, source.matrix);
-
-        //checking success
-        ErrorCodeException* exeption = new ErrorCodeException(er);
-        if(!exeption->isSuccess()) {
-            throw exeption;
-        }
+        //creating copy of the matrix in the field & throwing exception if needed
+        ErrorCodeException::throwErrorIfNeeded(matrix_copy(&matrix, source.matrix));
 
         return *this;
+    }
+
+    uint32_t getHeight() const throw(ErrorCodeException) {
+        uint32_t height;
+
+        //gets the height & throwing exception if needed
+        ErrorCodeException::throwErrorIfNeeded(matrix_getHeight(matrix, &height));
+
+        return height;
     }
 };
